c3a7c8a8e63bc6ed9fa28b72a222e04923ea8add
Mykle jones <mykle.j91@gmail.com>
2019-08-09 01:13:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,9 +25,12 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-if hash[key]
-  hash[key] += 1
-else 
-  hash[key] = 1 
-end
+ def update_hash
+   if hash[key]
+     hash[key]+=1 
+   else
+     hash[key]=1
+   end
+  
+
 end
